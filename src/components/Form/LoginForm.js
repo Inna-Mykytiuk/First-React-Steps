8ac59d22cdedd83fdd7c0cc54a3967f973f66792
Inna Mykytiuk <innka-pinnka@ukr.net>
2023-04-08 14:28:25 +0300

    form

--- src/components/Form/LoginForm.js
+++ src/components/Form/LoginForm.js
@@ -2,29 +2,53 @@ import { Component } from 'react';
 import { FormWrapper } from './LoginForm.styled';
 
 export class LoginForm extends Component {
+  state = {
+    loginValue: '',
+    passwordValue: '',
+  };
+
+  handleLoginChange = evt => {
+    this.setState({ loginValue: evt.target.value });
+  };
+
+  handlePasswordChange = evt => {
+    this.setState({ passwordValue: evt.target.value });
+  };
+
   handleSubmit = evt => {
     evt.preventDefault();
     const form = evt.currentTarget;
     const login = form.elements.login.value;
     const password = form.elements.password.value;
     console.log(login, password);
-    this.props.onSubmit({ login, password });
+
     form.reset();
   };
+
   render() {
+    const { loginValue, passwordValue } = this.state;
+
     return (
       <section>
         <FormWrapper>
           <h3>Login Form</h3>
           <form onSubmit={this.handleSubmit}>
-            <input type="text" name="login" placeholder="Enter your name" />
+            <input
+              type="text"
+              name="login"
+              placeholder="Enter your name"
+              value={loginValue}
+              onChange={this.handleLoginChange}
+            />
             <input
               type="password"
               name="password"
               placeholder="Enter your password"
+              value={passwordValue}
+              onChange={this.handlePasswordChange}
             />
+            <button type="submit">Log in</button>
           </form>
-          <button type="submit">Log in</button>
         </FormWrapper>
       </section>
     );
