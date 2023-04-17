3a9ac017278fa4d5bf38086393f9ccbe512aad0e
Inna Mykytiuk <innka-pinnka@ukr.net>
2023-04-17 21:27:07 +0300

    add hooks

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,84 +4,68 @@ import { Toaster } from 'react-hot-toast';
 
 import { Layout } from './Layout/Layout';
 import { Counter } from './Counter/Counter';
-import { Component } from 'react';
 import { LoginForm } from './Form/LoginForm';
 import { SignUpForm } from './Form/RegistrationForm';
 import { RecipeForm } from './RecipeForm/RecipeForm';
 import { Clock } from './Clock/Clock';
 import { AppWrapper } from './App.styled';
-// import { Modal } from './RecipeList/Modal';
 import { Search } from './Search/Search';
 import { NewsList } from './NewsList/NewsList';
-export class App extends Component {
-  //початковий стан. дані із файла json, наш масив об'єктів, який ми будемо змінювати
-  state = {
-    // recipes: InitialRecipes,
-    recipes: [],
-    // selectedImg: null,
-    textSearch: '',
-  };
+import { useState, useEffect } from 'react';
 
-  componentDidMount() {
-    const savedRecipes = localStorage.getItem('recipes');
-    if (savedRecipes !== null) {
-      const parsedRecipes = JSON.parse(savedRecipes);
-      this.setState({ recipes: parsedRecipes });
-      return;
-    }
-    this.setState({ recipes: InitialRecipes });
-
-    // const parsedRecipes = JSON.parse(recipes);
-    // if (parsedRecipes) {
-    //   this.setState({ recipes: parsedRecipes });
-    // }
+const getInitialRecipes = () => {
+  const savedRecipes = localStorage.getItem('recipes');
+  if (savedRecipes !== null) {
+    const parsedRecipes = JSON.parse(savedRecipes);
+    return parsedRecipes;
   }
+  return InitialRecipes;
+};
 
-  componentDidUpdate(prevProps, prevState) {
-    if (prevState.recipes !== this.state.recipes) {
-      localStorage.setItem('recipes', JSON.stringify(this.state.recipes));
-    }
-  }
+export const App = () => {
+  const [recipes, setRecipes] = useState(getInitialRecipes());
+  const [textSearch, setTextSearch] = useState('');
 
-  handleSubmit = textSearch => {
-    this.setState({ textSearch });
+  useEffect(() => {
+    localStorage.setItem('recipes', JSON.stringify(recipes));
+  }, [recipes]);
+
+  // const handleSubmit = searchText => {
+  //   setTextSearch(prevState => [...prevState, searchText]);
+  // };
+
+  const handleSubmit = searchText => {
+    setTextSearch(searchText);
   };
 
-  //створюємо метод видалення рецепту за id(бо він є унікальним)
-  deleteRecipe = id => {
-    this.setState(prevState => ({
-      recipes: prevState.recipes.filter(recipe => recipe.id !== id),
-    }));
+  const deleteRecipe = id => {
+    setRecipes(prevState => prevState.filter(recipe => recipe.id !== id));
   };
 
-  addRecipe = newRecipe => {
-    this.setState(prevState => ({
-      recipes: [...prevState.recipes, newRecipe],
-    }));
+  const addRecipe = newRecipe => {
+    setRecipes(prevState => [...prevState, newRecipe]);
   };
 
-  render() {
-    return (
-      <Layout>
-        <Toaster
-          position="top-right"
-          toastOptions={{
-            duration: 1500,
-          }}
-        />
-        <Search onSearch={this.handleSubmit} />
-        <NewsList value={this.state.textSearch} />
-        <RecipeList items={this.state.recipes} onDelete={this.deleteRecipe} />
-        <RecipeForm onSubmit={this.addRecipe} />
+  return (
+    <Layout>
+      <Toaster
+        position="top-right"
+        toastOptions={{
+          duration: 1500,
+        }}
+      />
+      <Search onSearch={handleSubmit} />
+      <NewsList searchText={textSearch} />
+      <RecipeList items={recipes} onDelete={deleteRecipe} />
+      <RecipeForm onSubmit={addRecipe} />
 
-        <AppWrapper>
-          <Counter />
-          <Clock />
-        </AppWrapper>
+      <AppWrapper>
+        <Counter />
+        <Clock />
+      </AppWrapper>
 
-        <LoginForm />
-        <SignUpForm />
-      </Layout>
-    );
-  }
-}
+      <LoginForm />
+      <SignUpForm />
+    </Layout>
+  );
+};
--- src/components/Recipe/Recipe.js
+++ src/components/Recipe/Recipe.js
@@ -3,7 +3,7 @@ import { BsAlarm } from 'react-icons/bs';
 import { AiOutlinePieChart } from 'react-icons/ai';
 import { HiOutlineChartBar, HiTrash, HiZoomIn } from 'react-icons/hi';
 import { Modal } from '../RecipeList/Modal';
-import { Component } from 'react';
+import { useState } from 'react';
 import {
   Container,
   Image,
@@ -18,87 +18,75 @@ import {
   Actions,
 } from './Recipe.styled';
 
-export class Recipe extends Component {
+export const Recipe = ({
+  item: { name, image, time, servings, calories, difficulty, id },
+  onDelete,
+}) => {
   //початковий стан для модального вікна, модальне вікно скрите
-  state = {
-    isOpen: false,
-  };
+  const [isOpen, setIsOpen] = useState(false);
 
   //створюємо методи для відкриття і закриття модального вікна
-  openModal = () => this.setState({ isOpen: true });
-  closeModal = () => this.setState({ isOpen: false });
+  const openModal = () => setIsOpen(true);
+  const closeModal = () => setIsOpen(false);
 
-  render() {
-    const {
-      item: { name, image, time, servings, calories, difficulty, id },
-      onDelete,
-    } = this.props;
+  return (
+    <Container>
+      <Image src={image} alt={name} />
+      <Meta>
+        <RecipeTitle>{name}</RecipeTitle>
 
-    return (
-      <Container>
-        <Image src={image} alt={name} />
-        <Meta>
-          <RecipeTitle>{name}</RecipeTitle>
+        <RecipeInfo>
+          <InfoItem>
+            <BsAlarm />
+            {time} min
+          </InfoItem>
+          <InfoItem>
+            <AiOutlinePieChart />
+            {servings} servings
+          </InfoItem>
+          <InfoItem>
+            <HiOutlineChartBar />
+            {calories} calories
+          </InfoItem>
+        </RecipeInfo>
 
-          <RecipeInfo>
-            <InfoItem>
-              <BsAlarm />
-              {time} min
-            </InfoItem>
-            <InfoItem>
-              <AiOutlinePieChart />
-              {servings} servings
-            </InfoItem>
-            <InfoItem>
-              <HiOutlineChartBar />
-              {calories} calories
-            </InfoItem>
-          </RecipeInfo>
+        <RecipeDifficulties>
+          <DifficultiesTitle>Difficulties</DifficultiesTitle>
+          <BadgeList>
+            <Badge isActive={difficulty === 'easy'} value={difficulty}>
+              Easy
+            </Badge>
+            <Badge isActive={difficulty === 'medium'} value={difficulty}>
+              Medium
+            </Badge>
+            <Badge isActive={difficulty === 'hard'} value={difficulty}>
+              Hard
+            </Badge>
+          </BadgeList>
+        </RecipeDifficulties>
 
-          <RecipeDifficulties>
-            <DifficultiesTitle>Difficulties</DifficultiesTitle>
-            <BadgeList>
-              <Badge isActive={difficulty === 'easy'} value={difficulty}>
-                Easy
-              </Badge>
-              <Badge isActive={difficulty === 'medium'} value={difficulty}>
-                Medium
-              </Badge>
-              <Badge isActive={difficulty === 'hard'} value={difficulty}>
-                Hard
-              </Badge>
-            </BadgeList>
-          </RecipeDifficulties>
+        <Actions>
+          {/* Ця кнопка відповідає за видалення елементу за ідентифікатором id, сам стейт з функцією розташований у App, а сюди передається пропрос-колбеком */}
+          <button
+            type="button"
+            onClick={() => onDelete(id)}
+            aria-label="Delete"
+          >
+            <HiTrash />
+          </button>
 
-          <Actions>
-            {/* Ця кнопка відповідає за видалення елементу за ідентифікатором id, сам стейт з функцією розташований у App, а сюди передається пропрос-колбеком */}
-            <button
-              type="button"
-              onClick={() => onDelete(id)}
-              aria-label="Delete"
-            >
-              <HiTrash />
-            </button>
+          {/* Ця кнопка відкриває модальне вікно і приводить isOpen до true*/}
+          <button type="button" onClick={openModal} aria-label="Zoom">
+            <HiZoomIn />
+          </button>
+        </Actions>
+      </Meta>
 
-            {/* Ця кнопка відкриває модальне вікно і приводить isOpen до true*/}
-            <button type="button" onClick={this.openModal} aria-label="Zoom">
-              <HiZoomIn />
-            </button>
-          </Actions>
-        </Meta>
-
-        {/* Якщо стан isOpen буде true тоді зарендериться розмітка модального вікна і з'явиться кнопка Close після натискання на яку значення isOpen буде приведено до false, відповідно розмітка зникне*/}
-        {this.state.isOpen && (
-          <Modal
-            image={image}
-            isOpen={this.state.isOpen}
-            onClose={this.closeModal}
-          />
-        )}
-      </Container>
-    );
-  }
-}
+      {/* Якщо стан isOpen буде true тоді зарендериться розмітка модального вікна і з'явиться кнопка Close після натискання на яку значення isOpen буде приведено до false, відповідно розмітка зникне*/}
+      {isOpen && <Modal image={image} isOpen={isOpen} onClose={closeModal} />}
+    </Container>
+  );
+};
 
 // Описуємо PropTypes для компонента Recipe
 Recipe.propTypes = {
